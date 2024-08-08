e2d1f2f8f00ab9411d17c8a7a6b2657911917f57
Donk3ylee <boylee.whiting@googlemail.com>
2024-08-08 13:02:15 +0100

    changed variable name to X as standard for input layer

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-# Now using a batch of inputs (list of lists) rather than a vector list
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+# changed the variable name to X as standard for the input layer in ML
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
@@ -21,9 +21,9 @@ biases2 = [-1, 2, -0.5]
 # Because we are now using a batch of inputs we need to transpose the weights
 # so that we dont get a shape error. Note we swapped the dot order too.
 
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer1_outputs = np.dot(X, np.array(weights).T) + biases
 layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
 
 print(layer2_outputs    )
 
-# UPTO pt.4 18.48
+# UPTO pt.4 24.05
